270752db98b6b0e7c4064cce45640ea3667664bf
Carlos Roberto <carlos2551@gmail.com>
2020-12-26 19:31:48 -0300

    Mensagem explicativa

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Ponto de entrada da aplicação
-		String frase = "Hi world!";
+		String frase = "Hi World!";
+		String frase2 = "Hi Github!";
 		
-		System.out.println(frase);
+		System.out.printf(frase + "%n" + frase2);
 	}
-
 }
